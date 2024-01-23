1da472b5fdb2813e3fd35d0c24f43463151fe26d
rocio <rramirez@birt.eus>
2024-01-24 00:43:08 +0100

    commit final socios

--- Socios/js/scripts.js
+++ Socios/js/scripts.js
@@ -25,7 +25,7 @@ cargarSociosJSON()
 /*
   funcion para leer del JSON
 */
-function cargarSociosJSON(){
+async function cargarSociosJSON(){
   let path = 'model/datosSocios.json'
   
   let request = new Request(path,{
@@ -36,7 +36,7 @@ function cargarSociosJSON(){
   fetch(request).then(response =>{
     response.json().then(async data => {
       console.log('Datos',data)
-      await aniadirSociosInicialesArray(data)
+       aniadirSociosInicialesArray(data)
     })
   })
 }
@@ -45,22 +45,54 @@ function cargarSociosJSON(){
 TODO:  metodo para añadir socios al array 
     cuando arranca la pagina web
 */
-async function aniadirSociosInicialesArray (datos) {
+ function aniadirSociosInicialesArray (datos) {
   //  TODO: cargar el fichero JSON, parsearlo a objetos tipo "socio" y añadirlos al array
+    
+    // // convertirlo con json
+      let jsonData = `[
+        {
+            "id":1,
+            "nombre": "Andoni",
+            "apellido": "Larrieta"
+        },
+    
+        {
+            "id":2,
+            "nombre": "Jon",
+            "apellido": "Agirre"
+        },
+    
+        {
+            "id":3,
+            "nombre": "Patxi",
+            "apellido": "Mujika"
+        }
+    
+    ]`
+    console.log(typeof(jsonData));
+    let jsonParseado = JSON.parse(jsonData);
 
-    let socios = JSON.parse(datos);
-
-   
-
-}
-
+    let i 
+    for(i= 0; i < jsonParseado.length; i++){
+      arraySocios.push(jsonParseado[i]);
+    }
+    //borrar
+    console.log('array',arraySocios)
+    }
 /*
     TODO: Meotodo para capturar los datos del socio introducidor en el formulario
 
 */
 function capturarDatosSocio () {
   // TODO: recoger los el nombre y apellido del HTML
+  let nombre = formulario.querySelector('#fnombre').value;
+  let apellido = formulario.querySelector('#fapellido').value;
+  
   // TODO: crear el socio y añadirlo al array
+  crearSocio(nombre,apellido)
+
+
+
 }
 
 /* 
@@ -69,8 +101,18 @@ TODO:
     y añadirlo al array
  */
 function crearSocio (nombre, apellido) {
+  let id = crearID()
+  
   // TODO: crear objeto socio
+  const nuevoSocio = {
+    id : crearID(),
+    nombre : nombre,
+    apellido : apellido
+  }
+  
   // TODO: añadir el objeto al array
+  arraySocios.push(nuevoSocio)
+  console.log('nuevoarray',arraySocios)
 }
 
 /*
@@ -80,7 +122,12 @@ TODO:
 */
 function crearID () {
   // TODO: mirar el id del ultimo socio del array y sumarle uno
+  
+  let ultimoId = arraySocios.length
+  return ultimoId + 1
+
 }
+
 
 // EJERCICIO 2
 
@@ -90,9 +137,20 @@ function crearID () {
 */
 function pintarListaSocios () {
   //TODO: borramos todo lo que hay en el div
+  contenedorEscribirSocios.innerHTML=''
   //TODO: bucle para recorrer y pintar el array de socios
-  //TODO: debemos añadir los socios a la pagina web
+  for(let i = 0; i < arraySocios.length; i++){
+    let socioActual = arraySocios[i]
+
+      //TODO: debemos añadir los socios a la pagina web
+    const nuevoElemento = document.createElement('div')
+
+    nuevoElemento.textContent=`Socio nº ${socioActual.id} ${socioActual.nombre} ${socioActual.apellido}`
+    contenedorEscribirSocios.appendChild(nuevoElemento)
+  }
+
 }
+
 
 // ------------------- MAIN ------------------------
 
